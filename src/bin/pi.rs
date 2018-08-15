24c3aaf3a68a1953b7d2172dbb3957ef258fc091
DD5HT <hendrik@dd5ht.de>
2018-08-15 14:23:27 +0200

    add CLI

--- src/bin/pi.rs
+++ src/bin/pi.rs
@@ -1,8 +1,26 @@
+#[macro_use]
+extern crate structopt;
 extern crate montepi;
+
+use structopt::StructOpt;
 
 use montepi::calc_pi;
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "pi")]
+struct Opt {
+
+    #[structopt(short = "s", long = "samples")]
+    samples: Option<u64>,
+}
+
 fn main(){
-    let samples = 100_000_000;
-    println!("π ~ {}",calc_pi(samples));
+    let opt = Opt::from_args();
+    if let Some(samples) = opt.samples {
+        println!("π ~ {}",calc_pi(samples));
+    }else {
+        let default = 10000;
+        println!("Using default value of {}:", default);
+        println!("π ~ {}",calc_pi(default));
+    }
 }
