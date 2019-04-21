65b46f987dfdb143837613be78d3e8d75553d7e2
DD5HT <hendrik@dd5ht.de>
2019-04-21 19:57:16 +0200

    update python version

--- src/bin/pi.py
+++ src/bin/pi.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
+def pi_calc_base(samples=100_000_000):
+    import random
+    import math
 
-import random
-import math
-
-
-def pi_calc_stock(samples=100000):
     count_inside = 0
     for count in range(samples):
         d = math.hypot(random.random(), random.random())
@@ -13,14 +11,12 @@ def pi_calc_stock(samples=100000):
     return 4.0 * count_inside / samples
 
 
-def pi_calc_nump(samples=100000000):
+def pi_calc_numpy(samples=100_000_000):
     import numpy as np
+
     count_inside = 0
     for count in range(samples):
         d = np.hypot(np.random.rand(), np.random.rand())
         if d < 1:
             count_inside += 1
     return 4.0 * count_inside / samples
-
-
-print(pi_calc_stock())
